b8dfed93ae48a634840909c50acc6b063c122fc1
leahkubo <kubalaleah@gmail.com>
2024-03-12 12:21:43 -0400

    Created and added second fragment

--- Assignment0/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ Assignment0/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.fragmentapp
+package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,7 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        supportFragmentManager.beginTransaction().add(R.id.fragmentContainerView2, ColorFragment())
     }
 }
